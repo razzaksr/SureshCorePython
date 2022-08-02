85783a8ee71018b2902076c57b502ec1e92a011b
razzaksr <razzaksr@gmail.com>
2022-08-02 10:13:30 +0530

    introduction and basic program

---
+++ first.py
@@ -0,0 +1,9 @@
+print("welcome zealous tech corp\t to become\n web developer")
+
+print("Where suresh gonna learn to develop following")
+
+print("console app using core python\n\tOOP\n\terror handling\n\tfiles\n\tdatabase")
+
+print("static web app using basic web technology\n\thtml\n\tcss\n\tjs\n\tbs")
+
+print("dynamic web app using django\n\tJinja\n\tmtv architecture\n\tdjango orm")
